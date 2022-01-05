47ab4820fde2f20da01b075c992c35732d7a03c1
hawaiianpizz4 <josue_morales_riv11@hotmail.com>
2022-01-05 09:57:40 -0500

    Agregando Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes,RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FooterComponent } from './footer/footer.component';
+import { BeneficiosComponent } from './beneficios/beneficios.component';
+import { IniciarsesionComponent } from './iniciarsesion/iniciarsesion.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+
+
+
+
+
+const navegacion:Routes=[
+  {path:'inicio', component: InicioComponent},
+  {path:'beneficios',component:BeneficiosComponent},
+  {path:'iniciarsesion',component:IniciarsesionComponent},
+  {path:'registrarse',component:RegistrarseComponent},
+];
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    FooterComponent,
+    BeneficiosComponent,
+    IniciarsesionComponent,
+    RegistrarseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(navegacion)
   ],
   providers: [],
   bootstrap: [AppComponent]
